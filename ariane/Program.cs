fff5329a5c7186244494f559d3957f416410c6a7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:12:09 +0100

    si chiede la velocità all'utente

--- ariane/Program.cs
+++ ariane/Program.cs
@@ -12,15 +12,18 @@ namespace ariane
         {
             double tronc = 0, tempo=0;
             double spazio = 0;
-            double velocità = 1676;
+            double velocità = 0;
             //errore di tempo fatto dalla base militare
             tronc = 0.000000095;
             tempo = tronc *100*60*60*10;
             Console.WriteLine($"il tempo totale è {tempo}");
             //spazio totale errato
+            Console.WriteLine("inserisci la velocità");
+            velocità = Convert.ToDouble(Console.ReadLine());
             spazio = velocità * tempo;
             Console.WriteLine($"lo spazio totale è {spazio}");
             Console.ReadLine();
+
         }
     }
 }
