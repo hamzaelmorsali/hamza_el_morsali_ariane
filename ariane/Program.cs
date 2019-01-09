20e52bf9275948b2cd496838a14cfead63fa014c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 08:49:38 +0100

    l'errore di troncamento riportato dalla base militare ha causato molti danni.siccome il computer ha troncato alla 24esimo bit è uscito un errore di conversione quindi alla fine si è ritrovato il tempo sbagliato e a sua volta lo spazio ricoperto

---
+++ ariane/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ariane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tronc = 0, tempo=0;
+            double spazio = 0;
+            double velocità = 1676;
+            //errore di tempo fatto dalla base militare
+            tronc = 0.000000095;
+            tempo = tronc *100*60*60*10;
+            Console.WriteLine($"il tempo totale è {tempo}");
+            //spazio totale errato
+            spazio = velocità * tempo;
+            Console.WriteLine($"lo spazio totale è {spazio}");
+            Console.ReadLine();
+        }
+    }
+}
